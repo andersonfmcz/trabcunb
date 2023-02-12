4c67b37d295948f0ab84c06b1f6a8a14eeb0a262
Anderson <andersonf.mcz@gmail.com>
2023-02-12 11:38:01 -0300

    novo arquivo add

--- prog2/dataatual.c
+++ prog2/dataatual.c
@@ -6,16 +6,15 @@ int main(void) {
     time_t mytime;
     mytime = time(NULL);
     struct tm tm = *localtime(&mytime);
-    printf("Data: %d_%d_%d__%d_%d\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_min, tm.tm_sec);
+//    printf("Data: %d_%d_%d__%d_%d\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_min, tm.tm_sec);
     char str1[50];
     strcpy(str1, "imagem_conhecido");
     char str2[50], str3[50];;
     sprintf(str2,"%d", tm.tm_mday);
     strcpy(str3,str2); strcat(str3,"_"); sprintf(str2,"%d",tm.tm_mon+1); strcat(str3,str2);
     strcat(str3,"_");  sprintf(str2,"%d",tm.tm_year+1900); strcat(str3,str2);
-//    strcpy(str3,str2); strcat(str3,"_"); sprintf(str2,"%d",tm.tm_mon+1); strcat(str3,str2);
- //   strcpy(str3,str2); strcat(str3,"_"); sprintf(str2,"%d",tm.tm_mon+1); strcat(str3,str2);
-    //strcpy(str2, ctime(&mytime));
+    strcat(str3,"__");  sprintf(str2,"%d",tm.tm_min); strcat(str3,str2);
+    strcat(str3,"_");  sprintf(str2,"%d",tm.tm_sec); strcat(str3,str2);
     strcat(str1,str3);
     printf("Data: %s\n", str1);
 }
