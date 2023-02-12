b3932ca7c6c3314c84f50a9082394a693220cdb0
Anderson <andersonf.mcz@gmail.com>
2023-02-12 11:28:01 -0300

    novo arquivo add

--- prog2/dataatual.c
+++ prog2/dataatual.c
@@ -4,12 +4,13 @@
 
 int main(void) {
     time_t mytime;
+    time(&mytime);
     mytime = time(NULL);
     struct tm tm = *localtime(&mytime);
     printf("Data: %d_%d_%d__%d_%d\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_min, tm.tm_sec);
     char str1[50];
     strcpy(str1, "imagem_conhecido");
     char str2[50];
-    sprintf(str2,"%d", tm);
+    sprintf(str2,"%d", ctime(&mytime));
     printf("Data: %s\n", str2);
 }
