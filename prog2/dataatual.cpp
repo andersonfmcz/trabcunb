558cf0d49f6b7d50f3962ad9492a3936da9872b8
Anderson <andersonf.mcz@gmail.com>
2023-02-12 12:50:01 -0300

    novo arquivo add

---
+++ prog2/dataatual.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+
+char *nomeData()
+{
+   time_t mytime;
+   mytime = time(NULL);
+   struct tm tm = *localtime(&mytime);
+   char str2[50];
+  // char *str3 = malloc(sizeof(char) * 50);
+   char *str3 = malloc(sizeof(char) * 50);
+   sprintf(str2,"%d", tm.tm_mday);
+   strcpy(str3,"_"); strcat(str3,str2);
+   strcat(str3,"_"); sprintf(str2,"%d",tm.tm_mon+1); strcat(str3,str2);
+   strcat(str3,"_");  sprintf(str2,"%d",tm.tm_year+1900); strcat(str3,str2);
+   strcat(str3,"__");  sprintf(str2,"%d",tm.tm_hour); strcat(str3,str2);
+   strcat(str3,"_");  sprintf(str2,"%d",tm.tm_min); strcat(str3,str2);
+   strcat(str3,"_");  sprintf(str2,"%d",tm.tm_sec); strcat(str3,str2);
+   return(str3);
+}
+
+int main(void) {
+    char str1[150];
+    strcpy(str1, "/home/alisson/Documentos/Trabalho_Final/imagem_conhecido");
+    strcat(str1, nomeData());
+    strcat(str1,".jpg");
+    printf("Nome do arquivo: %s\n", str1);
+}
