24dc111f840cdcfea1e489388f61cdf3b2fecfa9
Anderson <andersonf.mcz@gmail.com>
2023-02-12 11:12:01 -0300

    novo arquivo add

--- prog2/dataatual.c
+++ prog2/dataatual.c
@@ -5,5 +5,5 @@ int main(void) {
     time_t mytime;
     mytime = time(NULL);
     struct tm tm = *localtime(&mytime);
-    printf("Data: %d/%d/%d %d\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_sec);
+    printf("Data: %d_%d_%d__%d_%d\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_min, tm.tm_sec);
 }
