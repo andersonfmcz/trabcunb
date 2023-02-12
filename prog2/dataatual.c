bafac886430df9b59abd79241aaaa22c27d1d198
Anderson <andersonf.mcz@gmail.com>
2023-02-12 12:04:01 -0300

    novo arquivo add

--- prog2/dataatual.c
+++ prog2/dataatual.c
@@ -4,6 +4,9 @@
 
 char*nomeData()
 {
+   time_t mytime;
+   mytime = time(NULL);
+   struct tm tm = *localtime(&mytime);
    char str2[50], str3[50];
    sprintf(str2,"%d", tm.tm_mday);
    strcpy(str3,str2); strcat(str3,"_"); sprintf(str2,"%d",tm.tm_mon+1); strcat(str3,str2);
@@ -17,9 +20,6 @@ char*nomeData()
 }
 
 int main(void) {
-    time_t mytime;
-    mytime = time(NULL);
-    struct tm tm = *localtime(&mytime);
 //    printf("Data: %d_%d_%d__%d_%d\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_min, tm.tm_sec);
     char str1[50];
     strcpy(str1, "imagem_conhecido");
