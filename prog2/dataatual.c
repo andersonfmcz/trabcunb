2d664c133c9d5d327ab38bc4081e111ed4a5e5dd
Anderson <andersonf.mcz@gmail.com>
2023-02-12 12:09:01 -0300

    novo arquivo add

--- prog2/dataatual.c
+++ prog2/dataatual.c
@@ -21,9 +21,10 @@ char *nomeData()
 
 int main(void) {
 //    printf("Data: %d_%d_%d__%d_%d\n", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900, tm.tm_min, tm.tm_sec);
-    char str1[50];
+    char str1[50], nomeArq[50];
+    nomeArq = nomeData();
     strcpy(str1, "imagem_conhecido");
-    strcat(str1,nomeData);
+    strcat(str1, nomeArq);
     strcat(str1,".jpg");
     printf("Nome do arquivo: %s\n", str1);
 }
