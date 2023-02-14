8083f421708a843929389ced389ba2d21a40ceaa
anderson <andersonf.mcz@gmail.com>
2023-02-13 21:58:01 -0300

    novo arquivo add

--- progarqcsv/arqcsv.cpp
+++ progarqcsv/arqcsv.cpp
@@ -30,7 +30,7 @@ int main() {
     if(!fileExists(csvFile))
         writeCsvFile(csvFile, "header1", "header2", "header3");
 
-        if (!writeCsvFile(csvFile, i, naam, static_cast<float>(i * 3.5))) {
+        if (!writeCsvFile(csvFile, 0, naam, static_cast<float>(0 * 3.5))) {
             std::cerr << "Failed to write to file: " << csvFile << "\n";
         }
     return 0;
