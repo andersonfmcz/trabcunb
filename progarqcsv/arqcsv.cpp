c58d97822773cce82228319b0b886021ff7a7cb8
anderson <andersonf.mcz@gmail.com>
2023-02-13 21:37:01 -0300

    novo arquivo add

--- progarqcsv/arqcsv.cpp
+++ progarqcsv/arqcsv.cpp
@@ -5,7 +5,6 @@ using namespace std;
 /*Everytime before running this program delete previously created 'Student_data.csv' file to avoid buffer overflow errors*/
 int main()
 {
-    int roll_no;
     string name,college,class_of_student;
     
     fstream fout;
@@ -15,18 +14,9 @@ int main()
     fout<<"Name"<<","<<"College"<<","<<"Class"<<","<<"Roll no"<<"\n";
     
     cout<<"Enter details of 5 students=>";
- //   for(int i=1; i<=5; i++)
- //   {
-        name = "nome";
-        cout<<"College of student "<<i<<" :";
-        cout<<"Class of student "<<i<<" :";
-        getline(cin,class_of_student);
-        cout<<"Roll no of student "<<i<<" :";
-        cin>>roll_no;
-        cin.ignore();   /* if we use getline fun after cin function then it will skip 1 input. To avoid this we need to use 'cin.ignore()' fun to flush newline out of the buffer*/
+        str(name,"nome"); str(college,"colegio"); str(class_of_student, "estudante");
     
-        fout<<name<<","<<college<<","<<class_of_student<<","<<roll_no<<"\n";    // writing data to a csv file
-    }
+        fout<<name<<","<<college<<","<<class_of_student<<"\n";    // writing data to a csv file
     fout.close();   // closing csv file
     fstream fin;
     string data;
@@ -36,12 +26,6 @@ int main()
     // opening existing csv file in read mode        
     fin.open("Student_data.csv", ios::in);
     
-    for(int i=0; i<=5; i++)
-    {
-        // getline fun will read one entire row of csv file and will store it in string 'data'
-        getline(fin,data);
-        cout<<data<<"\n";
-    }
     
     return 0;
 }
