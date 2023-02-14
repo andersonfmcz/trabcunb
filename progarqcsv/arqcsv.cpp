21a25d65ae81e4229d41d4ed6542cf6f1481f692
anderson <andersonf.mcz@gmail.com>
2023-02-13 22:04:01 -0300

    novo arquivo add

--- progarqcsv/arqcsv.cpp
+++ progarqcsv/arqcsv.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <mutex>
 #include <fstream>
+#include <cstring>
 
 std::mutex logMutex;
 
@@ -8,15 +9,16 @@ bool fileExists(std::string& fileName) {
     return static_cast<bool>(std::ifstream(fileName));
 }
 
-template <typename filename, typename T1, typename T2, typename T3>
-bool writeCsvFile(filename &fileName, T1 column1, T2 column2, T3 column3) {
+template <typename filename, typename T1, typename T2, typename T3, typename T4>
+bool writeCsvFile(filename &fileName, T1 coluna1, T2 coluna2, T3 coluna3, T4 coluna4) {
     std::lock_guard<std::mutex> csvLock(logMutex);
     std::fstream file;
     file.open (fileName, std::ios::out | std::ios::app);
     if (file) {
-        file << "\"" << column1 << "\",";
-        file << "\"" << column2 << "\",";
-        file << "\"" << column3 << "\"";
+        file << "\"" << coluna1 << "\",";
+        file << "\"" << coluna2 << "\",";
+        file << "\"" << coluna3 << "\"";
+        file << "\"" << coluna4 << "\"";
         file <<  std::endl;
         return true;
     } else {
@@ -25,10 +27,12 @@ bool writeCsvFile(filename &fileName, T1 column1, T2 column2, T3 column3) {
 }
 
 int main() {
-    std::string csvFile = "logfile2.csv";
-    std::string naam = "Hallo";
+    std::string csvFile = "registrocam.csv";
+    std::string naam = "Olah";
+    char campo1[10];
+    strcpy(campo1,"NOME");
     if(!fileExists(csvFile))
-        writeCsvFile(csvFile, header1, header2, header3);
+        writeCsvFile(csvFile, campo1, "header2", "header3");
 
         if (!writeCsvFile(csvFile, 0, naam, static_cast<float>(0 * 3.5))) {
             std::cerr << "Failed to write to file: " << csvFile << "\n";
